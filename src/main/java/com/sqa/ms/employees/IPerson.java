41a8fc5b56097db7d937cd45987f44060733029f
mansham6 <mansour.shamsai@outlook.com>
2015-12-19 15:58:00 -0800

    Creating IPerson Interface

---
+++ src/main/java/com/sqa/ms/employees/IPerson.java
@@ -0,0 +1,27 @@
+/**
+ *   File Name: IPerson.java<br>
+ *
+ *   LastName, FirstName<br>
+ *   Java Boot Camp Exercise<br>
+ *   Instructor: Jean-francois Nepton<br>
+ *   Created: Dec 19, 2015
+ *   
+ */
+
+package com.sqa.ms.employees;
+
+/**
+ * IPerson //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ * 
+ * @author      LastName, FirstName
+ * @version     1.0.0
+ * @since       1.0
+ *
+ */
+public interface IPerson {
+
+}
